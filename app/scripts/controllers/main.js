7586683d406ed694d2ed7c0fe5094e96d7c3647a
David Ollerhead <david.ollerhead@gmail.com>
2013-11-11 16:57:31 +0000

    Initial scaffold and commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularGoodgymApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
