00260e7bc3c030b212009d5282c715a5bf1f6de1
David Ollerhead <david.ollerhead@gmail.com>
2013-12-04 16:29:55 +0000

    improved layout of runsList

--- test/spec/controllers/runDetail.js
+++ test/spec/controllers/runDetail.js
@@ -3,7 +3,7 @@
 describe('Controller: RundetailCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('angularGoodgymApp'));
+  beforeEach(module('goodgymApp'));
 
   var RundetailCtrl,
     scope;
