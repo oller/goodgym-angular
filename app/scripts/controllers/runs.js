89428a11e5330cdf911a3c52f5b1957760aed070
David Ollerhead <david.ollerhead@gmail.com>
2013-12-12 11:01:38 +0000

    noData directive on list of Runs, cloaking FOUC

--- app/scripts/controllers/runs.js
+++ app/scripts/controllers/runs.js
@@ -7,18 +7,16 @@ angular.module('goodgymApp')
 
       // Initial visibility of Create Run Form
       $scope.toggleCreate = false;
-      // Get All runs
-      // $scope.runs = Run.query();
 
       var runs = Run.query('', function success() {
         if (runs.group_runs) {
           $scope.runs = runs.group_runs;
         } else {
-          $scope.noData = true;
+          $scope.noDataPane = true;
         }
       }, function error() {
         toaster.pop('error', 'Uh-oh', 'There was an error in trying to retrieve the list of runs');
-        $scope.noData = true;
+        $scope.noDataPane = true;
       });
 
     }
